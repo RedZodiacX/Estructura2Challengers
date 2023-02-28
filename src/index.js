ba0700138a311481d13022f2496ba5fddf721d2a
RedZodiacX <locurax41@hotmail.com>
2023-02-28 18:57:23 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FirstApp } from './Components/Challenger/FirstApp';
+//import { FirstApp } from './Components/Challenger/FirstApp';
+import SaludoHora from './Components/Challenger/Taller-Punto3/SaludoHora';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <FirstApp />
+    <SaludoHora />
   </React.StrictMode>
 );
