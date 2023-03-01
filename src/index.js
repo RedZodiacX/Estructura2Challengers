90e8c8b02cedfbc470b5b65c348fd3d1dc4f0514
RedZodiacX <locurax41@hotmail.com>
2023-02-28 19:07:08 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FirstApp } from './Components/Challenger/FirstApp';
+//import { FirstApp } from './Components/Challenger/FirstApp';
+import ListaSalario from './Components/Challenger/Taller-Punto7/ListaSalario';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <FirstApp />
+    <ListaSalario />
   </React.StrictMode>
 );
