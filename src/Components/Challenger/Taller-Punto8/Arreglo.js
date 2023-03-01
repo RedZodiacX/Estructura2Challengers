2bc273e657b11198fcd65bdb8f367c8dad39d4bc
RedZodiacX <locurax41@hotmail.com>
2023-02-28 19:08:33 -0500

    Add files via upload

---
+++ src/Components/Challenger/Taller-Punto8/Arreglo.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Arreglo = () => {
+  const [numbers, setNumbers] = useState(
+    Array.from({ length: 10 }, () => Math.floor(Math.random() * 100))
+  );
+  const [dividerIndex, setDividerIndex] = useState("");
+
+  const divideNumbers = () => {
+    if (isNaN(dividerIndex) || dividerIndex < 0 || dividerIndex > 9) {
+      alert("Debe ingresar un índice válido");
+      return;
+    }
+    const divider = numbers[dividerIndex];
+    const dividedNumbers = numbers.map((number) => number / divider);
+
+    alert(`Números divididos: ${dividedNumbers}`);
+  };
+
+  return (
+    <div>
+      <h2>Punto 8</h2>
+      <p />
+      <div>
+        <h1>Arreglo de números aleatorios:</h1>
+        <ul>
+          {numbers.map((number, index) => (
+            <li key={index}>{number}</li>
+          ))}
+        </ul>
+        <input
+          type="number"
+          placeholder="Ingrese el índice para el divisor"
+          onChange={(e) => setDividerIndex(Number(e.target.value))}
+        />
+        <button onClick={divideNumbers}>Dividir números</button>
+      </div>
+    </div>
+  );
+};
+
+export default Arreglo;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FirstApp } from './Components/Challenger/FirstApp';
+//import { FirstApp } from './Components/Challenger/FirstApp';
+import Arreglo from './Components/Challenger/Taller-Punto8/Arreglo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <FirstApp />
+    <Arreglo />
   </React.StrictMode>
 );
