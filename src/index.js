813c0fa6c20196cddce5de3bdb000a2229f3dbff
RedZodiacX <locurax41@hotmail.com>
2023-02-28 19:02:43 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FirstApp } from './Components/Challenger/FirstApp';
+//import { FirstApp } from './Components/Challenger/FirstApp';
+import OrdenarNumeros from './Components/Challenger/Taller-Punto5/OrdenarNumeros';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <FirstApp />
+    <OrdenarNumeros />
   </React.StrictMode>
 );
